3a3601f12dcf950cc13b05c08906ca2f0ce4aa1e
Xiao Yao <yaoxiao89@gmail.com>
2018-05-06 11:50:47 -0400

    Create RandomRepresentable

---
+++ XYRandomKit/Random.swift
@@ -0,0 +1,28 @@
+//
+//  Random.swift
+//  XYRandomKit
+//
+//  Copyright © 2018 Xiao Yao. All rights reserved.
+//  See LICENSE.txt for licensing information
+//
+
+import Foundation
+
+// MARK: - RandomRepresentable
+
+protocol RandomRepresentable {
+    static var random: Self { get }
+    static func randoms(min: Int, max: Int) -> [Self]
+}
+
+extension RandomRepresentable {
+    
+    static func randoms(min: Int = 1, max: Int = 1 + Int(arc4random() % 10)) -> [Self] {
+        var randoms: [Self] = []
+        for _ in min...max {
+            randoms.append(random)
+        }
+        return randoms
+    }
+    
+}
