a2cda38b72ced2e8d1cedb64102384c1afbaa12e
Xiao Yao <yaoxiao89@gmail.com>
2018-05-06 11:48:12 -0400

    Create project

---
+++ XYRandomKit/XYRandomKit.h
@@ -0,0 +1,19 @@
+//
+//  XYRandomKit.h
+//  XYRandomKit
+//
+//  Copyright © 2018 Xiao Yao. All rights reserved.
+//  See LICENSE.txt for licensing information
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XYRandomKit.
+FOUNDATION_EXPORT double XYRandomKitVersionNumber;
+
+//! Project version string for XYRandomKit.
+FOUNDATION_EXPORT const unsigned char XYRandomKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XYRandomKit/PublicHeader.h>
+
+
